ad794eb8d8c5c036815b5a1c76e672dd50cc2de7
Henry Lyman <henrynlyman@gmail.com>
2025-04-21 13:16:58 -0600

    add rlcard_durak

--- human.py
+++ human.py
@@ -1,6 +1,21 @@
 # it is up to these functions to make sure all actions done are legal
 
 def defend(hand, field):
-
     
 def attack(hand, field):
+    print("current field:")
+    for allegation, beating in field.cards:
+        print(f"{allegation}: {beating}")
+
+    print("choose cards to attack with:")
+    for i, card in enumerate(hand):
+        print(f"{i}: {card}")
+
+    inp = input("choose numbers, space separated")
+
+    for i in inp.split(" "):
+        j = int(i)
+        if field.add_allegation(self.hand[i]):
+            self.hand[i] = None
+        
+
