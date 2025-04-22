a875c9fd954ab8e19efc6661e469f0b52e98a1e4
Henry Lyman <henrynlyman@gmail.com>
2025-04-22 13:14:56 -0600

    make durak judger

--- rlcard_durak/judger.py
+++ rlcard_durak/judger.py
@@ -1,25 +1,13 @@
 class DurakJudger:
     def __init__(self, np_random):
-        ''' Initialize a BlackJack judger class
-        '''
         self.np_random = np_random
-        self.rank2score = {"A":11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "T":10, "J":10, "Q":10, "K":10}
 
     def judge_round(self, player):
-        ''' Judge the target player's status
-
-        Args:
-            player (int): target player's id
-
-        Returns:
-            status (str): the status of the target player
-            score (int): the current score of the player
-        '''
         score = self.judge_score(player.hand)
-        if score <= 21:
-            return "alive", score
+        if player.hand.empty():
+            return "out", score
         else:
-            return "bust", score
+            return "in", score
 
     def judge_game(self, game, game_pointer):
         ''' Judge the winner of the game
@@ -28,27 +16,22 @@ class DurakJudger:
             game (class): target game class
         '''
         '''
-                game.winner['dealer'] doesn't need anymore if we change code like this
-
-                player bust (whether dealer bust or not) => game.winner[playerX] = -1
-                player and dealer tie => game.winner[playerX] = 1
-                dealer bust and player not bust => game.winner[playerX] = 2
-                player get higher score than dealer => game.winner[playerX] = 2
-                dealer get higher score than player => game.winner[playerX] = -1
+                game.winner[playerX] = 1 => This player is not the durak
+                game.winner[playerX] = -5 => This player is the durak
                 game.winner[playerX] = 0 => the game is still ongoing
-                '''
+        '''
+    
+        players_in = 0
+        for player in game.players[]:
+            if !player.hand.empty():
+                players_in += 1
 
-        if game.players[game_pointer].status == 'bust':
-            game.winner['player' + str(game_pointer)] = -1
-        elif game.dealer.status == 'bust':
-            game.winner['player' + str(game_pointer)] = 2
-        else:
-            if game.players[game_pointer].score > game.dealer.score:
-                game.winner['player' + str(game_pointer)] = 2
-            elif game.players[game_pointer].score < game.dealer.score:
-                game.winner['player' + str(game_pointer)] = -1
-            else:
-                game.winner['player' + str(game_pointer)] = 1
+        if players_in > 1:
+            game.winner[game_pointer] = 0
+        else if !game.players[game_pointer].hand.empty():
+            game.winner[game_pointer] = 1
+        else
+            game.winner[game_pointer] = -5
 
     def judge_score(self, cards):
         # big reward for being out
